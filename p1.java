8be55e07552a5e591c374bcd6f469b8514e1140f
AquaThornz <trackpixel0@gmail.com>
2026-07-02 22:21:19 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -1,5 +1,5 @@
 import java.util.* ;
-public class p1 {
+public class Series_Sum {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in) ;
         int x, n, sum=0 ;
